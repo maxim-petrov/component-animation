f012bdcac6a240b3bc38b5c723e76e87c8aa2a5f
Maxim Petrov <mail@maximpetrov.ru>
2025-03-14 15:53:24 +0300

    Move slider styles to css

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 import { Duration, Easing, Spring, createSpringConfig } from '../animations/tokens';
 import '../global.css';
+import '../styles/components/Slider.css';
 
 const Slider = ({ 
   min = 0, 
