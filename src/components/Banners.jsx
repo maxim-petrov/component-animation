034213790a981e0a2419c907ce2323e4192dda1b
Maxim Petrov <mail@maximpetrov.ru>
2025-03-20 07:51:30 +0300

    Update banner animation to css

--- src/components/Banners.jsx
+++ src/components/Banners.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-import { bannerHoverAnimation, bannerIconAnimation, bannerAppearAnimation, bannerImageAnimation } from '../animations/bannerAnimations';
+import '../animations/bannerAnimations.css';
 import '../global.css';
-import '../styles/components/Cards.css';
-import '../styles/typography.css';
 
 const Banner = ({ id, title, subtitle, index }) => {
   return (
-    <motion.div 
-      className="card-banner-f1b-0-0-4 card-medium-cfd-0-0-4 card-primary-c1a-0-0-4 card-clickable-77a-0-0-4 aMRDQ wOzPG" 
+    <div 
+      className="card-banner-f1b-0-0-4 card-medium-cfd-0-0-4 card-primary-c1a-0-0-4 card-clickable-77a-0-0-4 aMRDQ wOzPG animated-banner" 
       data-e2e-id={`banner-${id}`}
-      {...bannerHoverAnimation}
-      {...bannerAppearAnimation}
-      whileHover="hover"
     >
       <div className="card-icon-236-0-0-4">
-        <motion.div 
-          className="icon-root-864-6-0-3"
-          {...bannerIconAnimation}
-        >
+        <div className="icon-root-864-6-0-3 banner-icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
             <path 
               fill="currentColor" 
@@ -27,7 +18,7 @@ const Banner = ({ id, title, subtitle, index }) => {
               clipRule="evenodd"
             />
           </svg>
-        </motion.div>
+        </div>
       </div>
       <div className="card-content-884-0-0-4">
         <div className="tg-label-large-medium-3d8-7-0-3">
@@ -37,17 +28,16 @@ const Banner = ({ id, title, subtitle, index }) => {
           <div className="card-subTitle-75a-0-0-4">{subtitle}</div>
         </div>
         <div className="card-content-884-0-0-4">
-          <motion.img 
+          <img 
             src="https://statics.dmclk.ru/web-ui-library/images/topline/homeland-projects.svg" 
             alt="Проекты домов" 
             width="96" 
             height="96" 
-            className="_zBvS"
-            {...bannerImageAnimation}
+            className="_zBvS banner-image"
           />
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
@@ -60,24 +50,8 @@ const Banners = () => {
     { id: 51, title: '5-комнатная', subtitle: 'Подзаголовок' }
   ];
 
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.4,
-        delayChildren: 0.1
-      }
-    }
-  };
-
   return (
-    <motion.div 
-      className="QIMpV"
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
-    >
+    <div className="QIMpV banner-container">
       {bannersData.map((banner, index) => (
         <Banner 
           key={banner.id}
@@ -87,7 +61,7 @@ const Banners = () => {
           index={index}
         />
       ))}
-    </motion.div>
+    </div>
   );
 };
 
